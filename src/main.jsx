4131d51403546041377da0f77ed49858e6dea350
nonrita <2220025@ecc.ac.jp>
2024-04-26 12:17:23 +0900

    コンポーネントの使い方を知る

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-//コンポーネントの名前は大文字はじまり（パスカルケース）
-const App = () => {
-    return (
-        <> {/* 一つのタグで囲わなければならないというのを回避するためのもの　かっこいい･:*+.\(( °ω° ))/.:+ <React.Fragment> === <> */}
-            <h1>こんにちは</h1>
-            <p>お元気ですか？</p>
-        </>
-    );
-};
 
 root.render(
     <StrictMode> {/* reactの厳密なルールを適用する react推奨 */}
