edb75453cce27162e9d1478870a53d37901e7508
nonrita <2220025@ecc.ac.jp>
2024-04-27 09:43:20 +0900

    Propsを知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+import {ColorfulMessage} from "./components/ColorfulMessage";
+
 //コンポーネントの名前は大文字はじまり（パスカルケース） reactで決まっているので小文字はじまりの場合うまく表示されない
 export const App = () => { //exportをすることで他のファイルから参照できるようにしている
     const onClickButton = () => {alert()};
-    const contentStyle = {
-        color: "blue",
-        fontSize: "18px",
-        margin: 100
-    }
     return (
         <> {/* 一つのタグで囲わなければならないというのを回避するためのもの　かっこいい･:*+.\(( °ω° ))/.:+ <React.Fragment> === <> */}
             <h1 style={{ color: "red" }}>こんにちは</h1> {/* オブジェクトでstyelを指定できる*/}
-            <p style={contentStyle}>お元気ですか？</p>
+            <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+            <ColorfulMessage color="green">元気です！</ColorfulMessage>
             <button onClick={onClickButton}>ボタン</button>
         </>
     );
