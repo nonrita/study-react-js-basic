3379bf460f1217fc933d054c6c10e3822c136b8f
nonrita <2220025@ecc.ac.jp>
2024-04-27 09:24:10 +0900

    Reactでのイベントやスタイルの扱い方を知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 //コンポーネントの名前は大文字はじまり（パスカルケース） reactで決まっているので小文字はじまりの場合うまく表示されない
 export const App = () => { //exportをすることで他のファイルから参照できるようにしている
+    const onClickButton = () => {alert()};
+    const contentStyle = {
+        color: "blue",
+        fontSize: "18px",
+        margin: 100
+    }
     return (
         <> {/* 一つのタグで囲わなければならないというのを回避するためのもの　かっこいい･:*+.\(( °ω° ))/.:+ <React.Fragment> === <> */}
-            <h1>こんにちは</h1>
-            <p>お元気ですか？</p>
+            <h1 style={{ color: "red" }}>こんにちは</h1> {/* オブジェクトでstyelを指定できる*/}
+            <p style={contentStyle}>お元気ですか？</p>
+            <button onClick={onClickButton}>ボタン</button>
         </>
     );
 };
