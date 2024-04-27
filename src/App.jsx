ad35656b008f467fd81ee4e91c4b2f56abe89ddf
nonrita <2220025@ecc.ac.jp>
2024-04-27 21:49:37 +0900

    サイレンダリングと副作用を知る(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {ColorfulMessage} from "./components/ColorfulMessage";
 
 //コンポーネントの名前は大文字はじまり（パスカルケース） reactで決まっているので小文字はじまりの場合うまく表示されない
 export const App = () => { //exportをすることで他のファイルから参照できるようにしている
+    console.log("--App--");
     const [num, setNum] = useState(0);
+    const [isShowFace, setIsShowFace] = useState(false);
     const onClickCountUp = () => {
         setNum(( prev ) => prev + 1);
-        setNum(( prev ) => prev + 1);
     };
+    const onClickToggle = () => {
+        setIsShowFace(!isShowFace);
+    };
+
+    useEffect(() => {
+        if(num > 0){
+            if (num % 3 === 0) {
+                isShowFace || setIsShowFace(true);
+            } else {
+                isShowFace && setIsShowFace(false);
+            }
+        }
+    }, [num]);
+
     return (
         <> {/* 一つのタグで囲わなければならないというのを回避するためのもの　かっこいい･:*+.\(( °ω° ))/.:+ <React.Fragment> === <> */}
             <h1 style={{ color: "red" }}>こんにちは</h1> {/* オブジェクトでstyelを指定できる*/}
@@ -15,6 +30,8 @@ export const App = () => { //exportをすることで他のファイルから参
             <ColorfulMessage color="green">元気です！</ColorfulMessage>
             <button onClick={onClickCountUp}>カウントアップ</button>
             <p>{num}</p>
+            <button onClick={onClickToggle}>on/off</button>
+            {isShowFace && <p>test</p>}
         </>
     );
 };
